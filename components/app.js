747e1f2634c4064fcb4b879d13a56f1feeacd0ce
Sierra Henderson <henderson.sierra@yahoo.com>
2020-05-06 16:20:58 -0700

    Update handleGetGradesSuccess method

--- components/app.js
+++ components/app.js
@@ -8,7 +8,7 @@ class App {
     console.error(error);
   }
   handleGetGradesSuccess(grades) {
-    console.log(grades)
+    this.gradeTable.updateGrades(grades);
   }
   getGrades() {
     $.ajax({
