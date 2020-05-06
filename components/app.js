1dfecae204abbfd2c0d7e7f35faf27d160a969e9
Sierra Henderson <henderson.sierra@yahoo.com>
2020-05-06 15:59:58 -0700

    Added rest of class App methods

--- components/app.js
+++ components/app.js
@@ -1,5 +1,28 @@
 class App {
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
   handleGetGradesError(error) {
     console.error(error);
+  }
+  handleGetGradesSuccess(grades) {
+    console.log(grades)
+  }
+  getGrades() {
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      data: none,
+      headers:
+      {
+        "X-Access-Token": "J0aNGWGc"
+      },
+      success: this.handleGetGradesSuccess,
+      fail: this.handleGetGradesError
+    }),
+    start() {
+      this.getGrades();
+    }
   }
 }
