1f38668ce8b57b74a81bfbb46dfcf073b58f23df
Sierra Henderson <henderson.sierra@yahoo.com>
2020-05-07 12:44:00 -0700

    Modified the update method to use this.renderGradeRow

--- components/grade-table.js
+++ components/grade-table.js
@@ -7,17 +7,7 @@ class GradeTable {
     while (tBody.firstChild) {
       tBody.removeChild(tBody.firstChild);
     }
-    grades.forEach(function(el) {
-      var td1 = document.createElement('td');
-      td1.textContent = el.name;
-      var td2 = document.createElement('td');
-      td2.textContent = el.course;
-      var td3 = document.createElement('td');
-      td3.textContent = el.grade;
-      var tr = document.createElement('tr');
-      tr.append(td1, td2, td3);
-      tBody.append(tr);
-    })
+    grades.forEach(el => this.renderGradeRow(el, this.deleteGrade)
   }
   onDeleteClick(deleteGrade) {
     this.deleteGrade = deleteGrade
