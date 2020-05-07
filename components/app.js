793f205266ce771624ad9374346495a88c541169
Sierra Henderson <henderson.sierra@yahoo.com>
2020-05-07 13:12:28 -0700

    modified start method to pass this.deteGrade into onDeleteClick

--- components/app.js
+++ components/app.js
@@ -8,7 +8,7 @@ class App {
     this.createGrade = this.createGrade.bind(this);
     this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
     this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
-    this.DeleteGrade = this.DeleteGrade.bind(this);
+    this.deleteGrade = this.deleteGrade.bind(this);
     this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
     this.handleDeleteGradeSuccess = this.handleDeleteGradeSuccess.bind(this);
   }
@@ -40,6 +40,7 @@ class App {
   start() {
     this.getGrades();
     this.gradeForm.onSubmit(this.createGrade);
+    this.gradeTable.onDeleteClick(this.deleteGrade);
   }
   createGrade(name, course, grade) {
     $.ajax({
--- components/grade-table.js
+++ components/grade-table.js
@@ -8,9 +8,9 @@ class GradeTable {
     while (tBody.firstChild) {
       tBody.removeChild(tBody.firstChild);
     }
-    grades.forEach(el => this.renderGradeRow(el, this.deleteGrade)
+    grades.forEach(el => this.renderGradeRow(el, this.deleteGrade))
+    var noGrades = document.getElementById('noGrades')
     if (grades.length === 0) {
-      var noGrades = document.getElementById('noGrades')
       noGrades.classList.remove('d-none')
     } else {
       noGrades.className = 'd-none';
@@ -20,6 +20,7 @@ class GradeTable {
     this.deleteGrade = deleteGrade
   }
   renderGradeRow(data, deleteGrade) {
+    var tBody = this.tableElement.querySelector('tbody');
     var td1 = document.createElement('td');
     td1.textContent = data.name;
     var td2 = document.createElement('td');
@@ -29,12 +30,14 @@ class GradeTable {
     var td4 = document.createElement('td');
     var button = document.createElement('button');
     button.textContent = 'DELETE';
-    button.className = 'btn btn-success'
+    button.className = 'btn btn-danger'
     td4.append(button);
     var tr = document.createElement('tr');
     tr.append(td1, td2, td3, td4);
     tBody.append(tr);
-    button.addEventListener('click', this.deleteGrade(data.id));
+    button.addEventListener('click', function() {
+      deleteGrade(data.id)
+    });
     return tr;
   }
 }
