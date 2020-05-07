0b584fcf1984afbe5d14e8976b4564dab7f941c0
Sierra Henderson <henderson.sierra@yahoo.com>
2020-05-06 17:59:08 -0700

    Add pageHeader as arg to app

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ var tableElement = document.querySelector('table');
 
 var gradeTable = new GradeTable(tableElement)
 
-var app = new App(gradeTable);
+var app = new App(gradeTable, pageHeader);
 
 app.start();
