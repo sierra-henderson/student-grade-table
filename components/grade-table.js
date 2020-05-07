17e8bc25b743678e4d416daba1c5706150fdd3dc
Sierra Henderson <henderson.sierra@yahoo.com>
2020-05-07 12:18:14 -0700

    Created renderGradeRow element on GradeTable class

--- components/grade-table.js
+++ components/grade-table.js
@@ -22,4 +22,22 @@ class GradeTable {
   onDeleteClick(deleteGrade) {
     this.deleteGrade = deleteGrade
   }
+  renderGradeRow(data, deleteGrade) {
+    var td1 = document.createElement('td');
+    td1.textContent = data.name;
+    var td2 = document.createElement('td');
+    td2.textContent = data.course;
+    var td3 = document.createElement('td');
+    td3.textContent = data.grade;
+    var td4 = document.createElement('td');
+    var button = document.createElement('button');
+    button.textContent = 'DELETE';
+    button.className = 'btn btn-success'
+    td4.append(button);
+    var tr = document.createElement('tr');
+    tr.append(td1, td2, td3, td4);
+    tBody.append(tr);
+    button.addEventListener('click', this.deleteGrade(data.id));
+    return tr;
+  }
 }
