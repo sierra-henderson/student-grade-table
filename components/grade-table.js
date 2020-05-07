663fc7d812ccb95cdcd31b2faef5055d285a3c05
Sierra Henderson <henderson.sierra@yahoo.com>
2020-05-06 18:10:37 -0700

    Put average into the badge DOM element

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,7 +3,6 @@ class GradeTable {
     this.tableElement = tableElement
   }
   updateGrades(grades) {
-    console.log(grades)
     var tBody = this.tableElement.querySelector('tbody');
     while (tBody.firstChild) {
       tBody.removeChild(tBody.firstChild);
