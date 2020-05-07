a4e34c7e8e7d5a57985ae2f19748925552f11e9f
Sierra Henderson <henderson.sierra@yahoo.com>
2020-05-06 17:09:08 -0700

    Modified updateGrades to receive info from API and dynamically create tbody and fill with API Info

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,6 +3,21 @@ class GradeTable {
     this.tableElement = tableElement
   }
   updateGrades(grades) {
-    console.log(grades);
+    console.log(grades)
+    var tBody = this.tableElement.querySelector('tbody');
+    while (tBody.firstChild) {
+      tBody.removeChild(tBody.firstChild);
+    }
+    grades.forEach(function(el) {
+      var td1 = document.createElement('td');
+      td1.textContent = el.name;
+      var td2 = document.createElement('td');
+      td2.textContent = el.course;
+      var td3 = document.createElement('td');
+      td3.textContent = el.grade;
+      var tr = document.createElement('tr');
+      tr.append(td1, td2, td3);
+      tBody.append(tr);
+    })
   }
 }
